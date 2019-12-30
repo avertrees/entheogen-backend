cb6cb63ed068ba5ba85355fb80230e062837faa2
Alessandra Vertrees <avertrees447@gmail.com>
2019-12-29 20:24:37 -0500

    more indents

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,11 +15,11 @@ class ApplicationController < ActionController::API
         if auth_header
             token = auth_header.split(' ')[1]
             # header: { 'Authorization': 'Bearer <token>' }
-        begin
-            JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
-        rescue JWT::DecodeError
-            nil
-        end
+            begin
+                JWT.decode(token, 'my_s3cr3t', true, algorithm: 'HS256')
+            rescue JWT::DecodeError
+                nil
+            end
         end
     end
     
